f36c6b28443b2887cc46cc194d30e577904128e8
Claire-25 <136668541+Claire-25@users.noreply.github.com>
2025-10-15 11:31:19 -0700

    Initial Commit

---
+++ Run Wild/Run_WildApp.swift
@@ -0,0 +1,17 @@
+//
+//  Run_WildApp.swift
+//  Run Wild
+//
+//  Created by Claire Li on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Run_WildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
